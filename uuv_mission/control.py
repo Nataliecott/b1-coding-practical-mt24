ca75631dbae9194359c3f7aa1fd319cdff17b926
Natalie <nataliemaycotterill@gmail.com>
2025-10-19 11:51:05 +0100

    fourth-commit-changing gains

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,5 +1,5 @@
 class PDcontroller:
-    def __init__(self, kp: float = 0.15, kd: float = 0.6):
+    def __init__(self, kp: float = 0.18, kd: float = 0.75):
         self.kp = kp
         self.kd = kd
         self.prev_error = 0.0
